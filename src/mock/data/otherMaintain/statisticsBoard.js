56dac239126575f0b821716e23d1e861b887c00b
zhangpan <aimeep_7@163.com>
2019-03-12 15:52:23 +0800

    删除注释

--- src/mock/data/otherMaintain/statisticsBoard.js
+++ src/mock/data/otherMaintain/statisticsBoard.js
@@ -1,11 +1,6 @@
 import Mock from 'mockjs';
 
 const record = [];
-// const statusArr = [0, 1];
-// const equipType = ['井盖监测器', '烟感报警器'];
-// const dmpGrounp = ['烟感', '井盖'];
-// const phone = [135, 136, 137, 138, 139, 147, 150, 151, 152, 157, 158, 159, 187, 188,
-//   130, 131, 132, 155, 156, 185, 186, 133, 153, 180, 189];
 for (let i = 0; i < 30; i += 1) {
   const line = {
     deviceNum: parseInt((Math.random() * (10 * 3)), 10),
